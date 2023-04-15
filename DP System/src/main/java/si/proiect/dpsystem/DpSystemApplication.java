021d1ef78f16b871e35314b3c28c02b496a952b8
Tudor Pop <33669868+ptudorr@users.noreply.github.com>
2023-04-15 10:01:55 +0300

    spring boot application

---
+++ DP System/src/main/java/si/proiect/dpsystem/DpSystemApplication.java
@@ -0,0 +1,13 @@
+package si.proiect.dpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpSystemApplication.class, args);
+    }
+
+}
---
+++ DP System/src/main/java/si/proiect/dpsystem/MqttBeans.java
@@ -0,0 +1,65 @@
+package si.proiect.dpsystem;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import java.util.Objects;
+
+@Configuration
+public class MqttBeans {
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(new String[]{"tcp://192.168.0.115:1883"});
+        options.setCleanSession(false);
+        factory.setConnectionOptions(options);
+
+        return factory;
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter("springapplication", mqttClientFactory(), "dpsystem");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(2);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+
+    // Actual processing of the message arrived on the mqttInputChannel
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            String topic = Objects.requireNonNull(message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC)).toString();
+//            if (topic.equals("dpsystem")) {
+//                System.out.println("This is the topic");
+//            }
+            System.out.println(message.getPayload());
+        };
+    }
+
+}
